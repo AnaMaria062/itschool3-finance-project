5aab69859d85727b1c19ac35836a456343d94de2
AnaMaria <anaungureanu80@yahoo.com>
2023-04-29 16:41:50 +0300

    Add field descriptions for User, Asset, and UserInfo models

--- finance-project/api/models.py
+++ finance-project/api/models.py
@@ -6,17 +6,51 @@ class UserAdd(BaseModel):
     username: str = Field(description="Alphanumeric username between 6 and 20 chars")
 
 
-class UserInfo(BaseModel):
-    id: UUID
-    username: str
-    stocks: list[str]
-
-    class Config:
-        orm_mode = True
+class BaseConfig:
+    orm_mode = True
 
 
-class AssetInfo(BaseModel):
+class AssetAdd(BaseModel):
+    ticker: str = Field(description="The ticker symbol of the asset")
+
+
+class AssetInfoBase(BaseModel):
     ticker: str
-    units: float
-    name: str
-    country: str
+    name: str = Field(description="The name of the asset")
+    country: str = Field(description="The country where the asset is located")
+
+    class Config(BaseConfig):
+        pass
+
+
+class AssetInfoUser(AssetInfoBase):
+    units: float = Field(description="Number of units of the asset that the user owns")
+
+
+class AssetInfoPrice(AssetInfoBase):
+    current_price: float = Field(description="Current price of the asset")
+    currency: str = Field(description="The currency by which the price is expressed")
+    today_low_price: float = Field(
+        description="Lowest price of the asset during the current day"
+    )
+    today_high_price: float = Field(
+        description="Highest price of the asset during the current day"
+    )
+    open_price: float = Field(
+        description="Price of the asset at the beginning of the current day"
+    )
+    closed_price: float = Field(
+        description="Price of the asset at the end of the previous day"
+    )
+    fifty_day_price: float = Field(
+        description="Average price of the asset over the past fifty days"
+    )
+
+
+class UserInfo(BaseModel):
+    id: UUID = Field(description="Universally unique identifier for the user")
+    username: str
+    stocks: list[AssetInfoBase] = Field(description="List of assets that the user owns")
+
+    class Config(BaseConfig):
+        pass
