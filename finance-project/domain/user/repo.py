e0f625c7fa8ef31d072e1bbdef7f390840d5fe39
AnaMaria <anaungureanu80@yahoo.com>
2023-05-02 16:06:35 +0300

    Implement Singleton pattern for UserRepo

--- finance-project/domain/user/repo.py
+++ finance-project/domain/user/repo.py
@@ -1,39 +1,45 @@
 import logging
 import json
-
+import uuid
+from singleton import singleton
+from domain.asset.repo import AssetRepo
 from domain.user.factory import UserFactory
 from domain.user.persistance_interface import UserPersistenceInterface
 from domain.user.user import User
 
 
+@singleton
 class UserRepo:
     def __init__(self, persistence: UserPersistenceInterface):
+        print("Init user repo")
         self.__persistence = persistence
         self.logger = logging.getLogger(__name__)
         self.__users = None
         logging.basicConfig(filename='user_repo.log', level=logging.DEBUG)
 
-    def add(self, new_user: User):
-        # TODO homework, refactor to not have duplicate code + add for get_by_id
+    def __get_users(self):
         if self.__users is None:
             self.__users = self.__persistence.get_all()
-        self.__users.append(new_user)
+
+    def add(self, new_user: User):
+        self.__get_users()
         self.__persistence.add(new_user)
+        self.__users.append(new_user)
 
     def get_all(self) -> list[User]:
-        if self.__users is None:
-            self.__users = self.__persistence.get_all()
+        self.__get_users()
         return self.__users
 
-    def get_by_username(self, username: str) -> User:
-        for u in self.__users:
-            if u.username == username:
-                return u
-
     def get_by_id(self, user_id: str) -> User:
+        self.__get_users()
         for u in self.__users:
-            if u.id == user_id:
-                return u
+            if u.id == uuid.UUID(hex=user_id):
+                assets = AssetRepo().get_for_user(u)
+                return User(
+                    uuid=u.id,
+                    username=u.username,
+                    stocks=assets
+                )
 
     def delete(self, user_id: str):
         user_to_delete = self.get_by_id(user_id)
