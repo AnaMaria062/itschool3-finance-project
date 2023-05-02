5f2d47943ea7a0621fc1326edbe6d616089c8906
AnaMaria <anaungureanu80@yahoo.com>
2023-05-02 18:51:50 +0300

    Handle duplicate assets

--- finance-project/domain/asset/repo.py
+++ finance-project/domain/asset/repo.py
@@ -1,13 +1,14 @@
 import sqlite3
+
+from fastapi import HTTPException
+from starlette import status
+
 from domain.asset.asset import Asset
 from domain.user.user import User
 
 
 class AssetRepo:
     def add_to_user(self, user: User, asset: Asset):
-        # TODO homework, what happens if we already have this asset?
-        # sqlite3.IntegrityError: UNIQUE constraint failed: d27dda53_ddfc_40bc_99f8_cdd13f27b0d9_assets.ticker
-        # exception, 400 to api already added
         table = f"{user.id}-assets".replace("-", "_")
         with sqlite3.connect(f"main_users.db") as conn:
             cursor = conn.cursor()
@@ -15,14 +16,14 @@ class AssetRepo:
                 cursor.execute(f"INSERT INTO '{table}' (ticker, name, country, units) "
                                f"VALUES ('{asset.ticker}', '{asset.name}',"
                                f"'{asset.country}', {asset.units})")
-            except sqlite3.OperationalError:
-                cursor.execute(f"CREATE TABLE '{table}' "
-                               f"(ticker TEXT PRIMARY KEY, "
-                               f"name TEXT, country TEXT, units REAL)")
-                cursor.execute(f"INSERT INTO '{table}' (ticker, name, country, units) "
-                               f"VALUES ('{asset.ticker}', '{asset.name}',"
-                               f"'{asset.country}', {asset.units})")
-            conn.commit()
+            except sqlite3.IntegrityError:
+                conn.rollback()
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Asset already exists for this user"
+                )
+            else:
+                conn.commit()
 
     def get_for_user(self, user: User) -> list[Asset]:
         table = f"{user.id}-assets".replace("-", "_")
