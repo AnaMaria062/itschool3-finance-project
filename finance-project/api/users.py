b1c687199beca15e318c5ddd3e1967729de43495
AnaMaria <anaungureanu80@yahoo.com>
2023-05-02 16:00:34 +0300

    Add functionality to delete a user and to add an asset to a user

--- finance-project/api/users.py
+++ finance-project/api/users.py
@@ -1,51 +1,65 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from domain.asset.factory import AssetFactory
+from domain.asset.repo import AssetRepo
 from domain.user.repo import UserRepo
 from domain.user.factory import UserFactory
-from api.models import UserAdd, UserInfo, AssetInfoUser
+from api.models import UserAdd, UserInfo, AssetInfoUser, AssetAdd
 from persistence.user_file import UserPersistenceFile
+from persistence.user_sqlite import UserPersistenceSqlite
 
 users_router = APIRouter(prefix="/users")
 
-user_persistence = UserPersistenceFile("main_users.json")
-repo = UserRepo(user_persistence)
+
+def get_user_repo() -> UserRepo:
+    # user_persistence = UserPersistenceFile("main_users.json")
+    user_persistence = UserPersistenceSqlite()
+    return UserRepo(user_persistence)
 
 
 @users_router.get("", response_model=list[UserInfo])
-def get_all_users():
+def get_all_users(repo=Depends(get_user_repo)):
     return repo.get_all()
 
 
 @users_router.get("/{user_id}", response_model=UserInfo)
-def get_user(user_id: str):
+def get_user(user_id: str, repo=Depends(get_user_repo)):
     return repo.get_by_id(user_id)
 
 
-@users_router.get("/{username}", response_model=UserInfo)
-def get_user(username: str):
-    return repo.get_by_username(username)
-
-
 @users_router.post("", response_model=UserInfo)
-def create_a_user(new_user: UserAdd):
+def create_a_user(new_user: UserAdd, repo=Depends(get_user_repo)):
     user = UserFactory().make_new(new_user.username)
     repo.add(user)
     return user
 
 
 @users_router.delete("/{user_id}")
-def delete_user(user_id: str):
+def delete_user(user_id: str, repo=Depends(get_user_repo)):
     if repo.delete(user_id):
         return {"message": "User deleted successfully"}
     else:
         return {"message": "User not found"}
 
+
 # TODO fix api, return asset info
 
 
 @users_router.post("/{user_id}/assets", response_model=AssetInfoUser)
-def add_asset_to_user(user_id: str, ticker: str):
-    asset = AssetFactory().make_new(ticker)
-    print(asset.__dict__)
-    return asset
+def add_asset_to_user(user_id: str, asset: AssetAdd, repo=Depends(get_user_repo)):
+    try:
+        new_asset = AssetFactory().make_new(asset.ticker)
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid asset ticker provided",
+        )
+
+    user = repo.get_by_id(user_id)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+
+    AssetRepo().add_to_user(user, new_asset)
+    return new_asset
